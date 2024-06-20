4d173caeb2fa482fa8cc870007a6712745017cb8
missfazak <faezegholami97@gmail.com>
2024-06-20 15:02:19 +0330

    Initialize Project

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-css-tags */
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
